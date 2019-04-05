629cb5625e52857e84859ce9ecc8d790fb73d28d
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-04-05 18:01:14 -0400

    Fixed Car type

--- src/group3/finalproj/data/Car.java
+++ src/group3/finalproj/data/Car.java
@@ -10,16 +10,9 @@ class Car {
 	public TreeMap<Category, Object> categories = new TreeMap<Category, Object>();
 
 	protected Car(String[] categories, String[] values) {
-		/*Category order: Make, Model, Year, Price, BodyStyle, Trim, Fuel Type, CityMPG, HwMPG, Drivetrain, Trans, engine, mileage*, link
-		   *mileage only in used car database  */
 		for (int i = 0; i < categories.length; i++) {
 			this.categories.put(Category.valueOf(categories[i]), values[i]);
 		}
-		
-		for (String category: categories) {
-			
-		}
-
 	}
 
 	@Override
