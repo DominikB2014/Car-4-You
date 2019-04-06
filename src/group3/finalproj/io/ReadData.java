cc8ce6a9316b23d10fc6dd46758229a20dab623e
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-04-06 13:24:03 -0400

    Added a overload readCars method which reads cars within a price range.

--- src/group3/finalproj/io/ReadData.java
+++ src/group3/finalproj/io/ReadData.java
@@ -60,8 +60,8 @@ public class ReadData {
 					}
 					car = scanner.nextLine().split(",");
 				}
+				scanner.close();
 			}
-			scanner.close();
 			
 		}catch (FileNotFoundException e) {
 			System.out.println("File not Found");
