0fe48c7a69dbed6630ef92e2c249cbbf2057b781
Dominik Buszowiecki <DominikB2014@gmail.com>
2019-04-06 21:08:32 -0400

    Implemented CompareTo for Car

--- src/group3/finalproj/car/Car.java
+++ src/group3/finalproj/car/Car.java
@@ -13,23 +13,34 @@ public class Car {
 
 	/**
 	 * Constructs a car based on its categories
-	 * @param categories An array of categories
+	 * @param properties An array of categories
 	 * @param values A array of values, each corresponding to a category
 	 * in the categories array
 	 */
-	public Car(String[] categories, String[] values) {
-		for (int i = 0; i < categories.length; i++) {
+	public Car(String[] properties, String[] values) {
+		for (int i = 0; i < properties.length; i++) {
 			
 			// If the category is numeric, convert value to Integer and add to TreeMap
-			if(Property.isNumericProperty(Property.valueOf(categories[i]))) {
+			if(Property.isNumericProperty(Property.valueOf(properties[i]))) {
 				if (values[i].equals(""))
-					this.properties.put(Property.valueOf(categories[i]), 0);
-				else this.properties.put(Property.valueOf(categories[i]), Integer.parseInt(values[i]));
+					this.properties.put(Property.valueOf(properties[i]), 0);
+				else this.properties.put(Property.valueOf(properties[i]), Integer.parseInt(values[i]));
 			}
 			
 			//If the category is not numeric, add its string value			
-			else this.properties.put(Property.valueOf(categories[i]), values[i]);
+			else this.properties.put(Property.valueOf(properties[i]), values[i]);
 		}
+	}
+	
+	public int compareTo(Property property, Car car) {
+		Object a = properties.get(property);
+		Object b = car.properties.get(property);
+		if (Property.isNumericProperty(property)) {
+			if ((int)a == (int)b) return 0;
+			if ((int)a < (int)b) return -1;
+			return 1;
+		}
+		else return ((String)a).compareTo((String)b);
 	}
 	
 	public Object get(Property property) {
