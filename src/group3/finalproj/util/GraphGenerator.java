87c499d7824f16acc11ecbd19600ba977ba79bef
Pedram Yazdinia <yazdinp99@gmail.com>
2019-04-13 11:08:51 -0400

    Done doxygen

--- src/group3/finalproj/util/GraphGenerator.java
+++ src/group3/finalproj/util/GraphGenerator.java
@@ -6,7 +6,7 @@ import group3.finalproj.car.*;
 import group3.finalproj.io.ReadData;
 
 /**
- * 
+ * Graphing Algorithm required to recommend the user a compatible car
  * @author Pedram Yazdinia
  *
  */
@@ -14,6 +14,15 @@ public class GraphGenerator {
 	
 	public static ArrayList<Car> cars = ReadData.cars;
 	
+	/**
+	 * Main function that employs all of the helper functions 
+	 * to find and output the best five cars with the highest score.
+	 * @param property_rank An ArrayList of Tuples with the first value of type Property
+	 * and second value of type Integer
+	 * @param maxPrice The maxPrice that user chooses in the GUI
+	 * @param n The number of properties that our graph is going to connect the edges based on
+	 * @return carTuples An ArrayList of car with a maximum size of five.
+	 */
 	public static ArrayList<Car> masterScrum(ArrayList<Tuple<Property, Integer>> property_rank, int maxPrice, int n){
 		int source = findSource(property_rank, maxPrice);
 		try {
@@ -33,7 +42,13 @@ public class GraphGenerator {
 	   /***************************************************************************
 	    * Helper functions
 	    ***************************************************************************/
-	
+	/**
+	 * Graph generator function that goes through every Car in the initially filtered 
+	 * data set by the user specified conditions, to connect the cars that share the 
+	 * given number of properties or more than that.
+	 * @param n
+	 * @return
+	 */
 	private static Graph graphMake(int n) {
 		Graph G = new Graph(cars.size());
 		for (int i = 0; i < cars.size(); i++) {
@@ -54,7 +69,16 @@ public class GraphGenerator {
 		}
 		return G;
 	}
-	
+	/**
+	 * This method will run Depth First Search algorithm on a gien source to find all of the cars in the 
+	 * same connected component as the given source. 
+	 * @param G The graph in which the different connected components from our car dataset each share 
+	 * a certain number of properties.
+	 * @param source An integer that represents the source node which our DFS algorithm uses.
+	 * @param maxPrice An integer that specifies the max price given by the user.
+	 * @param property_Rank An ArrayList that contains custom Tuples of the type Property and Integer.
+	 * @return carTuples which represents all of the found cars in the same connected component by the DFS alg.
+	 */
 	private static ArrayList<Tuple<Car, Integer>> runDFS(Graph G, int source, int maxPrice, ArrayList<Tuple<Property, Integer>> property_Rank){
 		ArrayList<Tuple<Car, Integer>> carTuples = new ArrayList<Tuple<Car, Integer>>(); 
 		BreadthFirstPaths bfs = new BreadthFirstPaths(G, source);
@@ -68,7 +92,14 @@ public class GraphGenerator {
 		}
 		return carTuples;
 	}
-	
+	/**
+	 * This method returns the best five cars that have the highest score using the properties 
+	 * selected by the user. The program uses HeapSort to sort the given list of cars using their
+	 * score.
+	 * @param carTuples An ArrayList of Tuples that includes that properties associated with the 
+	 * attribute selected by the user in the program.
+	 * @return the cars with the 5 highest score. 
+	 */
 	private static ArrayList<Car> theBestFive(ArrayList<Tuple<Car, Integer>> carTuples){
 		System.out.println("Potential Cars: " + carTuples.size());
 		ArrayList<Car> bestCar = new ArrayList<Car>();
@@ -83,13 +114,25 @@ public class GraphGenerator {
 		}
 		return fiveBest;
 	}
-	
+	/**
+	 * Used to crossout properties that cannot be used to calculate a score. 
+	 * @param p Property to check.
+	 * @return True if the given property is not any of the following, BodyStyle, HighwayMPG, Link,
+	 * Transmission, Trim, Year, Model and FuelType.
+	 */
 	private static  boolean calcableProperty(Property p) {
 		return !(p.equals(Property.BodyStyle) || p.equals(Property.HighwayMPG) || p.equals(Property.Link) 
 				|| p.equals(Property.Transmission) || p.equals(Property.Trim) || p.equals(Property.Year)
 				|| p.equals(Property.Model) || p.equals(Property.FuelType));
 	}
-	
+	/**
+	 * This function will find a compatible source node in our data set by iterating through the 
+	 * cars dataset and checking if score of each car given a property_rank is bigger than the 
+	 * current score which is initialized to be 0. 
+	 * @param property_rank An ArrayList that contains custom Tuples of the type Property and Integer.
+	 * @param maxPrice An integer that specifies the max price given by the user.
+	 * @return An integer that represents the index of our source in our data set.
+	 */
 	private static int findSource(ArrayList<Tuple<Property, Integer>> property_rank, int maxPrice) {
 		int source = 0;
 		int total = totalRank(property_rank);
@@ -115,7 +158,12 @@ public class GraphGenerator {
 		cars = tempCars;
 		return source;
 	}
-	
+	/**
+	 * This method returns the sum of the weight that the user is given to each of their 
+	 * desired attributes.
+	 * @param property_rank An ArrayList that contains custom Tuples of the type Property and Integer.
+	 * @return An integer representing the total weight of the given attributes, selected by the user.
+	 */
 	private static int totalRank(ArrayList<Tuple<Property, Integer>> property_rank) {
 		int sum = 0;
 		for(Tuple<Property, Integer> tup: property_rank) {
