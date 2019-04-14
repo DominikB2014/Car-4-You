6a7fdc3ac5a314cabfbf6ead1e836d79c0d2af45
Pedram Yazdinia <yazdinp99@gmail.com>
2019-04-14 12:53:50 -0400

    Added doxygen for Tuple class

--- src/group3/finalproj/car/Tuple.java
+++ src/group3/finalproj/car/Tuple.java
@@ -3,14 +3,18 @@ package group3.finalproj.car;
 /**
  * 
  * @author Pedram Yazdinia
- *
- * @param <X>
- * @param <Y>
+ * A custom class to represent a tuple data structure.
+ * @param <X> Type for the first value of the Tuple
+ * @param <Y> Type for the second value of the Tuple
  */
 public class Tuple<X, Y> {
 	private final X property;
 	private final Y weight;
-	
+	/**
+	 * Constructor for Tuple ADT 
+	 * @param x First value of the Tuple of type X
+	 * @param y Second value of the Tuple of type Y
+	 */
 	public Tuple(X x, Y y) {
 		this.property = x;
 		this.weight = y;
@@ -20,10 +24,17 @@ public class Tuple<X, Y> {
 	public String toString () {
 		return property.toString() + " " + weight;
 }
+	/**
+	 * Get the first value of the tuple
+	 * @return property of Type X
+	 */
 	public X getProperty() {
 		return property;
 	}
-	
+	/**
+	 * Get the second value of the tuple
+	 * @return weight of type Y
+	 */
 	public Y getRank() {
 		return weight;
 	}
